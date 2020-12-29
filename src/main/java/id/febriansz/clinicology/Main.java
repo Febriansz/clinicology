bb8d7259adcba1a41be048df5bbe30e9187ac6c9
Muhammad Pebriyansyah <muhammad.pebriyansyah@fast-8.com>
2020-12-29 17:57:57 +0700

    added main class used for authentication

---
+++ src/main/java/id/febriansz/clinicology/Main.java
@@ -0,0 +1,16 @@
+package id.febriansz.clinicology;
+
+/**
+ *
+ * @author febriansz
+ */
+public class Main {
+
+    public Main() {
+
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
